cb948dafa84f178213f5fdfabfac6e67ce5337d9
xuantung159 <dxtung159@gmail.com>
2025-03-30 23:25:11 +0700

    Initial commit

---
+++ game.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include "doge.h"
+#include "pipe.h"
+#include "land.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class game:LTexture
+{
+public:
+    struct input
+    {
+        enum type { QUIT, PLAY, NONE, PAUSE};
+        type Type;
+    };
+    input userInput;
+    doge shiba;
+    pipe pipe;
+    land land;
+public:
+    game();
+
+    ~game();
+
+    bool initGraphic();
+
+    bool isQuit()
+    {
+        return quit;
+    }
+
+    bool isDie()
+    {
+        return die;
+    }
+
+    int getPipeWidth()
+    {
+        return pipe.width();
+    }
+
+    int getPipeHeight()
+    {
+        return pipe.height();
+    }
+
+    void takeInput();
+
+    void display();
+
+    void releaseGraphic();
+
+    void renderScoreSmall();
+
+    void renderScoreLarge();
+
+    void renderBestScore();
+
+    void renderMessage();
+
+    void renderBackground();
+
+    void renderBackgroundNight();
+
+    void renderLand();
+
+    void resume();
+
+    void pause();
+
+    void renderPauseTab();
+
+    void lightTheme();
+
+    void darkTheme();
+
+    void nextButton();
+
+    bool changeTheme();
+
+    void renderGameOver();
+
+    void renderMedal();
+
+    void replay();
+
+    bool checkReplay();
+
+    void Restart();
+
+private:
+    const double scaleNumberS = 0.75;
+	long long bestScore;
+};
